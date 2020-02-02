3d73b61019cdf612c0195a4c2483443309545c18
cjogles <jackson.ogles28@gmail.com>
2020-02-01 18:08:40 -0700

    Copied Abdi and I's work from today to my own branch and deleted node_modules. This way when we clone master we don't needlessly download node modules everytime unless we want to by typing the command npm i.

---
+++ data/migrations/20200201152634_users.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', user => {
+
+    user.increments();
+
+    user.string("email").notNullable();
+
+    user.string('password').notNullable();
+
+  })
+  
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("users");
+};
