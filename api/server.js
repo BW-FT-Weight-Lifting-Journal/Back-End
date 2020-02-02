4cce41aa4c35c04501154bf3d604fa2459ad1fda
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2020-02-02 15:57:44 -0800

    Authorization

--- api/server.js
+++ api/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
-const restricted = require("../auth/restrictedMiddleware.js");
+const restricted = require("../auth/restrictedMiddleware.js"); 
 const authRouter = require("../auth/authRouter.js");
 // const usersRouter = require("../users/userRouter.js");
-const workoutRouter = require("../workout/workoutRouter.js");
+// const workoutRouter = require("../workout/workoutRouter.js");
 
 const server = express();
 
@@ -15,7 +15,7 @@ server.use(express.json());
 
 server.use("/api/auth", authRouter);
 // server.use("/api/users", restricted, usersRouter); 
-server.use("/api/workout", restricted, workoutRouter);
+// server.use("/api/workout", restricted, workoutRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: "Server up!"})
--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,91 +1,81 @@
-// const router = require("express").Router();
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
-// const secrets = require("../config/secrets.js");
-// const Workout = require("../users/userModel.js");
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../config/secrets.js");
 
-// // Register Validation Middleware
-// const register = (req, res, next) => {
-//   const { email, password } = req.body;
-//   if (email && password) {
-//     next();
-//   } else {
-//     res.status(400).json({
-//       message: "Email and Password are required!"
-//     });
-//   }
-// };
+const Workout = require("../Routers/workoutModel.js.js");
 
-// // Login Validation Middleware
-// const login = (req, res, next) => {
-//   const { email, password } = req.body;
-//   if (email && password) {
-//     next();
-//   } else {
-//     res.status(400).json({
-//       message: "Email and Password are required!"
-//     });
-//   }
-// };
+// Register Validation Middleware
+const register = (req, res, next) => {
+  const { email, password } = req.body;
+  if (email && password) {
+    next();
+  } else {
+    res.status(400).json({
+      message: "Email and Password are required!"
+    });
+  }
+};
 
-// // Register End Point </api/auth/register> for ADDING a Workout
-// router.post("/register", register, (req, res) => {
+// Login Validation Middleware
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+  if (email && password) {
+    next();
+  } else {
+    res.status(400).json({
+      message: "Email and Password are required!"
+    });
+  }
+};
 
-//   const creds = req.body; // email, password, and id = creds
-//   const salt = bcrypt.genSaltSync(10); // salt password
-//   const hash = bcrypt.hashSync(creds.password, salt); // hash password and add salt
+// Register End Point </api/auth/register> for ADDING a Workout
+router.post("/register", register, (req, res) => {
 
-//   Workout.insert({ ...creds, password: hash })
-//     .then(() => {Workout.getBy({ email: creds.email })
-//       .then(user => {
-//         const token = generateToken(user);
-//         res.status(201).json({
-//           id: user.id,
-//           token
-//         });
-//       });
-//     })
-//     .catch(err => {
-//       res.status(500).json({ message: "", error: err });
-//     });
-// });
+  const creds = req.body; // email, password, and id = creds
+  const salt = bcrypt.genSaltSync(10); // salt password
+  const hash = bcrypt.hashSync(creds.password, salt); // hash password and add salt
+  creds.password = hash;
+
+  Workout.insert(creds)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 
-// router.post("/login", login, (req, res) => {
-//   const creds = req.body;
+router.post("/login", login, (req, res) => {
+  let { email, password } = req.body;
 
-//   Workout.getBy({ email: creds.email })
-//     .then(user => {
-//       //Check if passwords are the same
-//       if (user && bcrypt.compareSync(creds.password, user.password)) {
-//         const token = generateToken(user);
+  Workout.getBy({ email })
+    .first()
+    .then( user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
 
-//         newUser = { ...user, isParent: user.isParent === "1" ? true : false };
+        const token = generateToken(user);
 
-//         res.status(202).json({
-//           message: "Correct Credentials!",
-//           user: newUser,
-//           token
-//         });
-//       } else {
-//         res.status(401).json({ message: "Incorrect Credentials!" });
-//       }
-//     })
-//     .catch(err => {
-//       res.status(500).json({ message: "Database error", error: err });
-//     });
-// });
+        res.status(200).json({ token }); 
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
-// function generateToken(user) {
-//   const payload = {
-//     subject: user.id, 
-//     username: user.email
-//   };
+function generateToken(user) {
+  const payload = {
+     user
+  };
 
-//   const options = {
-//     expiresIn: "1d" 
-//   };
-//   return jwt.sign(payload, secrets.jwtSecret, options); 
-// }
+  const options = {
+    expiresIn: "1d" 
+  };
+  return jwt.sign(payload, jwtSecret, options); 
+}
 
-// module.exports = router;
+module.exports = router;
